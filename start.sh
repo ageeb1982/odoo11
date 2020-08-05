e7cc67fd9837fd99a3ad24d5502bf8487e8eaa7b
Mohamed khidir ageeb <ageeb1982@gmail.com>
2020-08-05 21:55:34 +0300

    First Commit 2020_08_05_21h_55m

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo -u ageeb /ageeb/ageeb-server/openerp-server --config=/etc/ageeb-server.conf
+#!/bin/sh
+sudo -u ageeb /ageeb/ageeb-server/openerp-server --config=/etc/ageeb-server.conf
